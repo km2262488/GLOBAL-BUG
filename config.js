8eab67f5a3ac45e027da2499b1e0cfc0bc458052
km2262488 <km2262488@gmail.com>
2024-11-30 13:13:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-global.namaown = "Qasim Ali" // your owner name here
+global.namaown = "MIYAZAWA" // your owner name here
 global.namabot = "© GlobalTechInfo" // your bot name here
 global.versisc = "9.5.0" //
-global.owner = ["923444844060"] // your owner number here
+global.owner = ["62882019150277"] // your owner number here
 global.tele = "https://t.me/GlobalBotInc" // your telegram here
 global.url = "https://www.youtube.com/@GlobalTechInfo" // Your YouTube Here
-global.namastore = "LynnZxD" // UBAH NAMA STORE LU
+global.namastore = "Barbarian" // UBAH NAMA STORE LU
 global.simbol = "✪" // You Can Change Simbol
 global.wlcm = []
 global.wlcmm = []
